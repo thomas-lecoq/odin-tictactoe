709b503ded2bca408605e457f4e5e09fc42f2467
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-06-13 12:40:22 +0200

    feat(gameboard): add cell ticking logic

--- js/gameboard.js
+++ js/gameboard.js
@@ -5,7 +5,17 @@ export function createGameBoard() {
     // board generation: 2D grid
     const [nRows, nCols] = [3, 3];
     const board = Array.from({ length: nRows }, () => 
-        Array.from({length: nCols}, () => [])
+        Array.from({length: nCols}, () => null)
     );
-    return board
+
+    const getBoard = () => board;
+    const getCell = (rowIdx, colIdx) => board[rowIdx][colIdx];
+    const setCell = (rowIdx, colIdx, symbol) => {
+        // if the cell is already taken, stop the operation
+        if (getCell(rowIdx, colIdx) !== null) return;
+        // set the symbol for a defined cell
+        board[rowIdx][colIdx] = symbol;
+    }
+
+    return { getBoard, setCell }
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 // main function
+import { createGameBoard } from "./gameboard.js";
 
-import { } from './utils.js';
-import { } from './config.js';
+function init() {
+    const board = createGameBoard();
+    console.log(board.getBoard());
+    board.setCell(0, 0, "X");
+    board.setCell(0, 1, "X");
+    board.setCell(0, 2, "X");
+    console.log(board.getBoard());
+}
 
 init();
