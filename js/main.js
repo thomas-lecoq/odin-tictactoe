54cca8e47528c63246af1e58856b05338a9983b7
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-06-20 19:43:50 +0200

    refactor(main): delegate game execution to playGame

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
 // main function
-import { createPlayerRegistry } from "./players.js";
-import { createGameBoard } from "./gameboard.js";
+import { playGame } from "./gamelogic.js"
 
 function init() {
-    const playerRegistry = createPlayerRegistry()
-    const players = playerRegistry.getPlayers();
-    const board = createGameBoard();
-
-    // simulate a game
-    console.log(board.getBoard());
-    board.setCell(players[0].playMove(0, 0));
-    board.setCell(players[1].playMove(0, 1));
-    board.setCell(players[0].playMove(0, 2));
-    console.log(board.getBoard());
+    playGame();
 }
 
 init();
