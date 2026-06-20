09d9819622fd47161faf6e37175a466753cdc81f
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-06-20 17:44:33 +0200

    style(gameboard): tidy whitespace

--- js/gameboard.js
+++ js/gameboard.js
@@ -1,7 +1,6 @@
 // gameboard related logic
 
 export function createGameBoard() {
-
     // board generation: 2D grid
     const [nRows, nCols] = [3, 3];
     const board = Array.from({ length: nRows }, () => 
